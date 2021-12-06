5ee10642efc843160afbd131d8f6255b719e70af
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2021-12-06 04:14:37 +0300

    Теперь, рессивер -- глобальный. Обслуживает включение/выключение блютуз, состояние сопряжения. Вызывается из Application()

--- app/src/main/java/com/grandfatherpikhto/blescan/service/BCReceiver.kt
+++ app/src/main/java/com/grandfatherpikhto/blescan/service/BCReceiver.kt
@@ -1,6 +1,10 @@
 package com.grandfatherpikhto.blescan.service
 
+import android.app.Application
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -9,6 +13,10 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 @DelicateCoroutinesApi
@@ -16,37 +24,71 @@ import kotlinx.coroutines.launch
 object BCReceiver : BroadcastReceiver() {
     const val TAG:String = "BCReceiver"
 
+    private var bluetoothManager:BluetoothManager? = null
+    private var bluetoothAdapter: BluetoothAdapter? = null
+
+    private var bondingDevice:BluetoothDevice? = null
+
+    private val _paired = MutableStateFlow<BluetoothDevice?>(null)
+    val paired = _paired.asSharedFlow()
+
+    private val _btState = MutableStateFlow<Int>(-1)
+    val btState = _btState.asStateFlow()
+
+
+    /**
+     * Проблема в том, что ACTION_BOND_STATE_CHANGED вызывается не только
+     * после сопряжения устройства, но и при каждом подключении
+     * Поэтому, при запросе на сопряжение сначала записываем устройство
+     * в переменную bondingDevice.
+     * Далее, когда приходит событие ACTION_BOND_STATE_CHANGED проверяем
+     * был ли перед этим запрос на сопряжение. Если был, генерируется событие
+     * paired. По этому событию вызывается попытка подключения.
+     */
     override fun onReceive(context: Context?, intent: Intent?) {
         // Log.d(TAG, "broadcastReceiver: ${intent?.action}")
+        if (bluetoothManager == null) {
+            bluetoothManager = context?.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        }
+        if (bluetoothAdapter == null) {
+            bluetoothAdapter = bluetoothManager?.adapter
+        }
+        
         if(intent != null) {
             when(intent.action) {
+                BluetoothAdapter.ACTION_STATE_CHANGED -> {
+                    val state    = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                    _btState.tryEmit(state)
+                    Log.d(TAG, "ACTION_STATE_CHANGED($state) ${state == BluetoothAdapter.STATE_ON}")
+                }
                 BluetoothDevice.ACTION_PAIRING_REQUEST -> {
-                    GlobalScope.launch {
-                        // sharedPairing.tryEmit(Pairing.PairingRequest)
-                    }
+                    bondingDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    Log.d(TAG, "ACTION_PAIRING_REQUEST(${bondingDevice?.address})")
                 }
                 BluetoothDevice.ACTION_BOND_STATE_CHANGED -> {
                     val bondState:Int
                         = intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, -1);
                     val previousBondState:Int
                         = intent.getIntExtra(BluetoothDevice.EXTRA_PREVIOUS_BOND_STATE, -1);
+                    val device: BluetoothDevice?
+                        = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
                     // BluetoothDevice.BOND_NONE    10
                     // BluetoothDevice.BOND_BONDING 11
                     // BluetoothDevice.BOND_BONDED  12
-                    // Log.d(TAG, "Изменился статус сопряжения ${sharedAddress.value}: $bondState, $previousBondState")
-                    if(previousBondState == BluetoothDevice.BOND_NONE
+                    // Log.d(TAG, "ACTION_BOND_STATE_CHANGED(${device?.address}): $previousBondState => $bondState")
+                    bondingDevice?.let {
+                        if( bondingDevice?.address == device?.address
+                            && previousBondState == BluetoothDevice.BOND_BONDING
                             && bondState == BluetoothDevice.BOND_BONDED) {
-                        GlobalScope.launch {
-                            // TODO: Разобраться, почему всё-время идёт уведомление, о том, что BOND_STATE_CHANGED
-                            Log.d(TAG, "Устройство было сопряжено. Запустить рескан $bondState $previousBondState")
-                            // sharedPairing.tryEmit(Pairing.Paired)
-                            // doRescanDevice()
+                            GlobalScope.launch {
+                                Log.d(TAG, "Устройство было сопряжено. PreviousBondState: $previousBondState, BondState: $bondState, device: ${device?.address}")
+                                _paired.tryEmit(device)
+                                bondingDevice = null
+                            }
                         }
                     }
                 }
                 BluetoothDevice.ACTION_FOUND -> {
-                    // Log.d(TAG, "Устройство найдено")
-                    // connect()
                 }
                 else -> {
 
