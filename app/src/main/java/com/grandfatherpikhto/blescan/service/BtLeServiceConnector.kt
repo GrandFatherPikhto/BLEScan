a34e448d550f31eaaa35a4c1c6a37f640d805a2a
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2021-12-07 07:06:59 +0300

    Объект (синглтон). BtLeScanServiceConnector.kt удалён. Все действия и сканирование и подключение идут теперь через этот объект

--- app/src/main/java/com/grandfatherpikhto/blescan/service/BtLeServiceConnector.kt
+++ app/src/main/java/com/grandfatherpikhto/blescan/service/BtLeServiceConnector.kt
@@ -1,18 +1,14 @@
 package com.grandfatherpikhto.blescan.service
 
 import android.bluetooth.BluetoothGatt
-import android.bluetooth.BluetoothGattService
 import android.content.ComponentName
 import android.content.ServiceConnection
-import android.os.Build
 import android.os.IBinder
-import android.util.Log
-import androidx.annotation.RequiresApi
+import com.grandfatherpikhto.blescan.model.BtLeDevice
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -25,8 +21,8 @@ object BtLeServiceConnector:ServiceConnection {
     private var btLeService:BtLeService ?= null
     val service:BtLeService? get() = btLeService
 
-    private var sharedBond = MutableStateFlow<Boolean>(false)
-    val bond: StateFlow<Boolean> = sharedBond
+    private var sharedBound = MutableStateFlow<Boolean>(false)
+    val bound: StateFlow<Boolean> = sharedBound
 
     private val sharedState = MutableStateFlow(BtLeService.State.Disconnected)
     val state: StateFlow<BtLeService.State> = sharedState
@@ -34,11 +30,18 @@ object BtLeServiceConnector:ServiceConnection {
     private val sharedGatt = MutableStateFlow<BluetoothGatt?>(null)
     val gatt = sharedGatt.asStateFlow()
 
+    private val sharedDevice = MutableSharedFlow<BtLeDevice?>(replay = 10)
+    val device = sharedDevice.asSharedFlow()
+
+    private val sharedEnabled = MutableStateFlow<Boolean>(false)
+    val enabled = sharedEnabled.asStateFlow()
+
+
     override fun onServiceConnected(p0: ComponentName?, serviceBinder: IBinder?) {
         // Log.d(TAG, "Сервис подключён")
         btLeService = (serviceBinder as BtLeService.LocalBinder).getService()
         GlobalScope.launch {
-            sharedBond.tryEmit(true)
+            sharedBound.tryEmit(true)
             btLeService?.state?.collect { value ->
                 sharedState.tryEmit(value)
             }
@@ -48,14 +51,40 @@ object BtLeServiceConnector:ServiceConnection {
                 sharedGatt.tryEmit(value)
             }
         }
+        GlobalScope.launch {
+            btLeService?.device?.collect{ found ->
+                sharedDevice.tryEmit(found)
+            }
+        }
+        GlobalScope.launch {
+            btLeService?.enabled?.collect { enabled ->
+                sharedEnabled.tryEmit(enabled)
+            }
+        }
     }
 
     override fun onServiceDisconnected(p0: ComponentName?) {
         GlobalScope.launch {
-            sharedBond.tryEmit(false)
+            sharedBound.tryEmit(false)
         }
     }
 
     fun connect(address:String) = btLeService?.connect(address)
+    fun connect(btLeDevice:BtLeDevice) = btLeService?.connect(btLeDevice)
     fun close() = btLeService?.close()
+
+    fun scanLeDevices( addresses: String? = null,
+                       names: String? = null,
+                       mode: BtLeScanner.Mode = BtLeScanner.Mode.FindAll)
+        = btLeService?.scanLeDevices(addresses, names, mode)
+
+    fun scanLeDevices( addresses: Array<String> = arrayOf<String>(),
+                       names: Array<String> = arrayOf<String>(),
+                       mode: BtLeScanner.Mode = BtLeScanner.Mode.FindAll)
+            = btLeService?.scanLeDevices(addresses, names, mode)
+
+    fun stopScan() = btLeService?.stopScan()
+
+    fun pairedDevices() = btLeService?.pairedDevices()
+
 }
