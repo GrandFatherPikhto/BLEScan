7c1aa8aa63a86563a78992bb706d914ecf82e45a
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2021-12-07 07:02:19 +0300

    Убран вызов слушателя широковещательных сообщений. Теперь, он принадлежит сервису BtLeService.kt

--- app/src/main/java/com/grandfatherpikhto/blescan/BleScanApp.kt
+++ app/src/main/java/com/grandfatherpikhto/blescan/BleScanApp.kt
@@ -1,46 +1,20 @@
 package com.grandfatherpikhto.blescan
 
 import android.app.Application
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.content.IntentFilter
 import android.util.Log
-import com.grandfatherpikhto.blescan.service.BcReceiver
 
 class BleScanApp: Application() {
     companion object {
-        const val TAG:String = "BleTestApp"
+        const val TAG: String = "BleTestApp"
     }
 
     override fun onCreate() {
         super.onCreate()
-        applicationContext.registerReceiver(BcReceiver, makeIntentFilter())
 
         Log.d(TAG, "OnCreate()")
     }
 
     override fun onTerminate() {
-        applicationContext.unregisterReceiver(BcReceiver)
         super.onTerminate()
     }
-
-    /**
-     * Создаём фильтр перехвата для различных широковещательных событий
-     * В данном случае, нужны только фильтры для перехвата
-     * В данном случае, нужны только фильтры для перехвата
-     * запроса на сопряжение устройства и завершения сопряжения
-     * И интересует момент "Устройство найдено" на случай рескана устройств
-     * по адресу или имени
-     */
-    private fun makeIntentFilter(): IntentFilter {
-        val intentFilter = IntentFilter()
-        intentFilter.addAction(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED)
-        intentFilter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED)
-        intentFilter.addAction(BluetoothDevice.ACTION_PAIRING_REQUEST)
-        intentFilter.addAction(BluetoothDevice.ACTION_FOUND)
-
-        return intentFilter
-    }
-
 }
