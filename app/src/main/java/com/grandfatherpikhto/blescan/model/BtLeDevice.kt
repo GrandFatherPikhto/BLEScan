9246473d36ea17b93820263496b67d99ac823308
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2021-12-12 04:10:14 +0300

    Модель фрагментов сканирования (ScanFragment.kt), и фрагмента подключения (DeviceFragment.kt). Подключается напрямую к синглтону BluetoothInterface.kt

--- app/src/main/java/com/grandfatherpikhto/blescan/model/BtLeDevice.kt
+++ app/src/main/java/com/grandfatherpikhto/blescan/model/BtLeDevice.kt
@@ -2,4 +2,11 @@ package com.grandfatherpikhto.blescan.model
 
 import android.bluetooth.BluetoothDevice
 
-data class BtLeDevice(val address: String, val name: String?, val bondState: Int)
+data class BtLeDevice(
+    val address: String = "00:00:00:00:00:00",
+    val name: String? = "Unknown Device",
+    val bondState: Int = BluetoothDevice.BOND_NONE)
+
+fun BluetoothDevice.toBtLeDevice(defaultAddress: String = "00:00:00:00:00:00", defaultName:String = "Unknown Device"): BtLeDevice {
+    return BtLeDevice(this.address ?: defaultAddress, this.name ?: defaultName, this.bondState)
+}
