bc90c168754f7d61b51cf8008874fb4585d03a5d
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2021-12-12 04:05:28 +0300

    Синглтон для подключения к сервису. Некий хитрый почти Room. Через него происходит обмен данными между всеми окнами приложения и сервисом.

--- app/src/main/java/com/grandfatherpikhto/blescan/service/BtLeServiceConnector.kt
+++ app/src/main/java/com/grandfatherpikhto/blescan/service/BtLeServiceConnector.kt
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothGatt
 import android.content.ComponentName
 import android.content.ServiceConnection
 import android.os.IBinder
+import android.util.Log
 import com.grandfatherpikhto.blescan.model.BtLeDevice
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
@@ -16,75 +17,34 @@ import kotlinx.coroutines.flow.*
 
 @InternalCoroutinesApi
 @DelicateCoroutinesApi
-object BtLeServiceConnector: ServiceConnection {
-    private const val TAG = "BtLeServiceConnector"
+class BtLeServiceConnector: ServiceConnection {
+    companion object {
+        private const val TAG = "BtLeServiceConnector"
+    }
+
     private var btLeService:BtLeService ?= null
     val service:BtLeService? get() = btLeService
 
-    private var sharedBound = MutableStateFlow<Boolean>(false)
-    val bound: StateFlow<Boolean> = sharedBound
-
-    private val sharedState = MutableStateFlow(BtLeService.State.Disconnected)
-    val state: StateFlow<BtLeService.State> = sharedState
-
-    private val sharedGatt = MutableStateFlow<BluetoothGatt?>(null)
-    val gatt = sharedGatt.asStateFlow()
-
-    private val sharedDevice = MutableSharedFlow<BtLeDevice?>(replay = 10)
-    val device = sharedDevice.asSharedFlow()
-
-    private val sharedEnabled = MutableStateFlow<Boolean>(false)
-    val enabled = sharedEnabled.asStateFlow()
-
+    /** */
+    private val bluetoothInterface:BluetoothInterface by BluetoothInterfaceLazy()
 
     override fun onServiceConnected(p0: ComponentName?, serviceBinder: IBinder?) {
-        // Log.d(TAG, "Сервис подключён")
+        Log.d(TAG, "Сервис подключён")
         btLeService = (serviceBinder as BtLeService.LocalBinder).getService()
-        GlobalScope.launch {
-            sharedBound.tryEmit(true)
-            btLeService?.state?.collect { value ->
-                sharedState.tryEmit(value)
-            }
-        }
-        GlobalScope.launch {
-            btLeService?.gatt?.collect { value ->
-                sharedGatt.tryEmit(value)
-            }
-        }
-        GlobalScope.launch {
-            btLeService?.device?.collect{ found ->
-                sharedDevice.tryEmit(found)
-            }
-        }
-        GlobalScope.launch {
-            btLeService?.enabled?.collect { enabled ->
-                sharedEnabled.tryEmit(enabled)
-            }
-        }
+        bluetoothInterface.service = btLeService
     }
 
     override fun onServiceDisconnected(p0: ComponentName?) {
-        GlobalScope.launch {
-            sharedBound.tryEmit(false)
-        }
+        bluetoothInterface.service = null
     }
 
-    fun connect(address:String) = btLeService?.connect(address)
-    fun connect(btLeDevice:BtLeDevice) = btLeService?.connect(btLeDevice)
-    fun close() = btLeService?.close()
+    override fun onNullBinding(name: ComponentName?) {
+        super.onNullBinding(name)
+        bluetoothInterface.service = null
+    }
 
-    fun scanLeDevices( addresses: String? = null,
-                       names: String? = null,
-                       mode: BtLeScanner.Mode = BtLeScanner.Mode.FindAll)
-        = btLeService?.scanLeDevices(addresses, names, mode)
-
-    fun scanLeDevices( addresses: Array<String> = arrayOf<String>(),
-                       names: Array<String> = arrayOf<String>(),
-                       mode: BtLeScanner.Mode = BtLeScanner.Mode.FindAll)
-            = btLeService?.scanLeDevices(addresses, names, mode)
-
-    fun stopScan() = btLeService?.stopScan()
-
-    fun pairedDevices() = btLeService?.pairedDevices()
-
+    override fun onBindingDied(name: ComponentName?) {
+        super.onBindingDied(name)
+        bluetoothInterface.service = null
+    }
 }
