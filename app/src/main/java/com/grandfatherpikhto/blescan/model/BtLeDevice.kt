9b1a8ca920080c12377ef5e1f0abe603ba57afe9
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2021-12-13 09:26:15 +0300

    Data-класс для упрощённого хранения и передачи данных BluetoothDevice

--- app/src/main/java/com/grandfatherpikhto/blescan/model/BtLeDevice.kt
+++ app/src/main/java/com/grandfatherpikhto/blescan/model/BtLeDevice.kt
@@ -7,6 +7,10 @@ data class BtLeDevice(
     val name: String? = "Unknown Device",
     val bondState: Int = BluetoothDevice.BOND_NONE)
 
+/**
+ * Сопроводительный конвертер для преобразования экземпляра BluetoothDevice
+ * в упрощённую "выжимку", достаточную для работы с устройством
+ */
 fun BluetoothDevice.toBtLeDevice(defaultAddress: String = "00:00:00:00:00:00", defaultName:String = "Unknown Device"): BtLeDevice {
     return BtLeDevice(this.address ?: defaultAddress, this.name ?: defaultName, this.bondState)
 }
