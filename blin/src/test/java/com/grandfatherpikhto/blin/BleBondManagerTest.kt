08e4dce46484cd27608311830c0164d38ca94617
Grandfather Pikhto <GrandfatherPikhto@gmail.com>
2022-08-20 19:34:15 +0300

    Класс, тестирования класса сопряжения с устройствами BleBondManager

--- blin/src/test/java/com/grandfatherpikhto/blin/BleBondManagerTest.kt
+++ blin/src/test/java/com/grandfatherpikhto/blin/BleBondManagerTest.kt
@@ -7,7 +7,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.test.core.app.ApplicationProvider
 import com.grandfatherpikhto.blin.data.BleBondState
-import com.grandfatherpikhto.blin.data.BleDevice
+import com.grandfatherpikhto.blin.orig.AbstractBleBondManager
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
@@ -53,7 +53,6 @@ class BleBondManagerTest {
     @Before
     fun setUp() {
         closeable = MockitoAnnotations.openMocks(this)
-        appCompatActivity.lifecycle.addObserver(bleBondManager)
     }
 
     @After
@@ -87,13 +86,13 @@ class BleBondManagerTest {
         val bluetoothDevice = shadowBluetoothDevice(address, NAME)
         shadowOf(bluetoothDevice).setCreatedBond(true)
         bleBondManager.bondRequest(address)
-        assertEquals(BleBondState(BleDevice(bluetoothDevice),
-            BleBondManager.State.Request), bleBondManager.bondState)
+        assertEquals(BleBondState(bluetoothDevice,
+            AbstractBleBondManager.State.Request), bleBondManager.bondState)
         shadowAdapter.setBondedDevices(mutableSetOf(bluetoothDevice))
         shadowOf(bluetoothDevice).setBondState(BluetoothDevice.BOND_BONDED)
         putIntentDevice(bluetoothDevice)
         ShadowLooper.shadowMainLooper().idle()
-        assertEquals(BleBondState(BleDevice(bluetoothDevice), BleBondManager.State.Bonded),
+        assertEquals(BleBondState(bluetoothDevice, AbstractBleBondManager.State.Bonded),
             bleBondManager.bondState)
     }
 
@@ -106,8 +105,8 @@ class BleBondManagerTest {
         shadowOf(bluetoothDevice).setCreatedBond(false)
         bleBondManager.bondRequest(address)
         ShadowLooper.shadowMainLooper().idle()
-        assertEquals(BleBondState(BleDevice(bluetoothDevice),
-            BleBondManager.State.Error), bleBondManager.bondState)
+        assertEquals(BleBondState(bluetoothDevice,
+            AbstractBleBondManager.State.Error), bleBondManager.bondState)
     }
 
     @Test
@@ -116,13 +115,13 @@ class BleBondManagerTest {
         val bluetoothDevice = shadowBluetoothDevice(address, NAME)
         shadowOf(bluetoothDevice).setCreatedBond(true)
         bleBondManager.bondRequest(address)
-        assertEquals(BleBondState(BleDevice(bluetoothDevice),
-            BleBondManager.State.Request), bleBondManager.bondState)
+        assertEquals(BleBondState(bluetoothDevice,
+            AbstractBleBondManager.State.Request), bleBondManager.bondState)
         shadowAdapter.setBondedDevices(mutableSetOf(bluetoothDevice))
         shadowOf(bluetoothDevice).setBondState(BluetoothDevice.BOND_NONE)
         putIntentDevice(bluetoothDevice, BluetoothDevice.BOND_NONE)
         ShadowLooper.shadowMainLooper().idle()
-        assertEquals(BleBondState(BleDevice(bluetoothDevice), BleBondManager.State.Reject),
+        assertEquals(BleBondState(bluetoothDevice, AbstractBleBondManager.State.Reject),
             bleBondManager.bondState)
     }
 }
