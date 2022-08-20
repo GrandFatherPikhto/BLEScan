3133c677091b29cb159b8caf5b5e329ae3eaf8e2
Grandfather Pikhto <GrandfatherPikhto@gmail.com>
2022-08-20 19:34:41 +0300

    Класс данных состояния сопряжения устройства BLE

--- blin/src/main/java/com/grandfatherpikhto/blin/data/BleBondState.kt
+++ blin/src/main/java/com/grandfatherpikhto/blin/data/BleBondState.kt
@@ -1,5 +1,24 @@
 package com.grandfatherpikhto.blin.data
 
-import com.grandfatherpikhto.blin.BleBondManager
+import android.bluetooth.BluetoothDevice
+import com.grandfatherpikhto.blin.orig.AbstractBleBondManager
 
-data class BleBondState (val bleDevice: BleDevice, val state: BleBondManager.State)
+data class BleBondState (val bluetoothDevice: BluetoothDevice, val state: AbstractBleBondManager.State) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BleBondState
+
+        if (bluetoothDevice.address != other.bluetoothDevice.address) return false
+        if (state != other.state) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = bluetoothDevice.hashCode()
+        result = 31 * result + state.hashCode()
+        return result
+    }
+}
