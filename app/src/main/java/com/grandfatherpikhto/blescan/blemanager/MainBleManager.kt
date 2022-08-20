327889dcc82152e1570de6c62ff51a6a7a4e46a9
Grandfather Pikhto <GrandfatherPikhto@gmail.com>
2022-08-20 19:55:35 +0300

    Пустой класс работы с BLE-устройством. Наследуется от абстрактного AppBleManager

---
+++ app/src/main/java/com/grandfatherpikhto/blescan/blemanager/MainBleManager.kt
@@ -0,0 +1,8 @@
+package com.grandfatherpikhto.blescan.blemanager
+
+import android.content.Context
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+class MainBleManager (context: Context)
+    : AppBleManager(context)
