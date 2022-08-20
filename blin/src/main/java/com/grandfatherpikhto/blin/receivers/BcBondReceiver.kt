66370d87244d89151efae3d3fbb431d46596c84b
Grandfather Pikhto <GrandfatherPikhto@gmail.com>
2022-08-20 19:21:42 +0300

    Рессивер событий сопряжения устройства

--- blin/src/main/java/com/grandfatherpikhto/blin/receivers/BcBondReceiver.kt
+++ blin/src/main/java/com/grandfatherpikhto/blin/receivers/BcBondReceiver.kt
@@ -4,11 +4,11 @@ import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.grandfatherpikhto.blin.BleBondManager
+import com.grandfatherpikhto.blin.orig.AbstractBleBondManager
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
-class BcBondReceiver constructor(private val bleBondManager: BleBondManager,
+class BcBondReceiver constructor(private val bleBondManager: AbstractBleBondManager,
                                  private val dispatcher: CoroutineDispatcher = Dispatchers.IO)
     : BroadcastReceiver() {
 
@@ -18,9 +18,9 @@ class BcBondReceiver constructor(private val bleBondManager: BleBondManager,
         if ( context != null && intent != null ) {
             when (intent.action) {
                 BluetoothDevice.ACTION_BOND_STATE_CHANGED -> {
-                    val bondState: Int = intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, -1);
+                    val bondState: Int = intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, -1)
                     val previousBondState: Int =
-                        intent.getIntExtra(BluetoothDevice.EXTRA_PREVIOUS_BOND_STATE, -1);
+                        intent.getIntExtra(BluetoothDevice.EXTRA_PREVIOUS_BOND_STATE, -1)
                     val bluetoothDevice: BluetoothDevice? =
                         intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
                     // Log.d(TAG, "ACTION_BOND_STATE_CHANGED(${device?.address}): $previousBondState => $bondState")
